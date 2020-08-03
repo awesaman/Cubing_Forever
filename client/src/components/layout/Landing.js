45d1347cdb345d7b95568faa86163857ce3ce576
Aman Singh <aman@little-mac.local>
2020-08-02 19:30:30 -0500

    Landing Page Complete

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
-    <div className='landing'>
-      <h1>CUBING FOREVER</h1>
-    </div>
+    <section className='landing'>
+      <div className='landing-inner'>
+        <h1 className='XL'>CUBING FOREVER</h1>
+        <p className='L'>
+          Create an account to find friends and battle against them!
+        </p>
+        <div className='buttons'>
+          <Link to='/register' className='btn btn-primary'>
+            Sign Up
+          </Link>
+          <Link to='/login' className='btn btn-light'>
+            Login
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 };
 
