3d3c6a7a161387717ed9fef4d1e006928033ff02
Aman Singh <aman@little-mac.local>
2020-08-02 23:39:25 -0500

    Added bottom row icons

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -17,6 +17,27 @@ const Landing = () => {
             Login
           </Link>
         </div>
+        <br />
+        <div className='extras'>
+          <div>
+            <Link to='/tutorials'>
+              <i className='fas fa-chalkboard-teacher' />
+              <p className='L text-light'>LEARN</p>
+            </Link>
+          </div>
+          <div>
+            <Link to='/timer'>
+              <i className='fas fa-stopwatch' />
+              <p className='L text-light'>PRACTICE</p>
+            </Link>
+          </div>
+          <div>
+            <Link to='/compete'>
+              <i className='fas fa-users' />
+              <p className='L text-light'>COMPETE</p>
+            </Link>
+          </div>
+        </div>
       </div>
     </section>
   );
