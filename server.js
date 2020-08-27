6ebe7ec9cf128600d5866ba1c7c2b32189163139
Aman Singh <aman@little-mac.local>
2020-08-27 01:01:52 -0500

    Deployment Test 11

--- server.js
+++ server.js
@@ -17,7 +17,6 @@ app.use(
 );
 
 app.use(cors());
-
 app.use(express.json());
 
 // Set Static Folder in Production Build
@@ -120,5 +119,9 @@ io.on('connection', socket => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.set('port', process.env.PORT || 5000);
+module.exports = app;
+
+app.listen(app.get('port'), function () {
+  console.log('Server listening on port ' + app.get('port'));
+});
