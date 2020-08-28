19da4e4525591732305515c5da4bc7b0eef72d01
Aman Singh <aman@little-mac.local>
2020-08-28 11:45:38 -0500

    Deploy to Heroku Test 4

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, { wsEngine: 'ws', forceNew: true });
 const bodyParser = require('body-parser');
+const path = require('path');
 connectDB();
 
 app.use(bodyParser.json({ limit: '50mb', extended: true }));
